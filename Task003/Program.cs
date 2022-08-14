c3f837f4362a0696dd2734a7d39944156d8be023
PrivalovArtem <rashen95@mail.ru>
2022-08-14 23:13:39 +0300

    Решена третья задача

---
+++ Task003/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void cubeNumber(double a)
+{
+    int number = 1;
+    if (a <= 0 || a % 1 > 0)
+    {
+        Console.WriteLine("Введено неверное число");
+    }
+    else
+    {
+        while (number < a)
+        {
+            Console.Write($"{Math.Pow(number, 3)}, ");
+            number++;
+        }
+        Console.Write($"{Math.Pow(number, 3)}");
+    }
+}
+
+Console.Write("Введите любое целое положительное число, а я выведу все числа до него включительно в кубе: ");
+double number = Convert.ToDouble(Console.ReadLine());
+cubeNumber(number);
